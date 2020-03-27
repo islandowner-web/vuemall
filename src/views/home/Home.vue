793cd604cd8f30169b9f2316f13b18e0e5afe5af
薛振 <xuezhen@xuezhendeMacBook-Pro.local>
2020-03-27 22:21:18 +0800

    day0327 homepage

---
+++ src/views/home/Home.vue
@@ -0,0 +1,123 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
+
+
+    <ul>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+    </ul>
+  </div>
+</template>
+
+<script>  
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+  import FeatureView from './childComps/FeatureView'
+
+  import NavBar from 'components/common/navbar/NavBar';
+  import TabControl from 'components/content/tabControl/TabControl';
+
+  import {getHomeMultidata} from "network/home";
+  
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      // 请求多个数据
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    },
+  }
+</script>
+
+<style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
+  .home-nav {
+    background: var(--color-tint);
+    color: white;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 3;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+  }
+</style>
